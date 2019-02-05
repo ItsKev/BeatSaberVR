b43c67de2685bf3aff4f43b137c71b1290467b0a
Kevin Huber <kevin.huber@hotmail.com>
2019-02-05 16:20:30 +0100

    Initial commit.

---
+++ BeatSaber/Assets/BeatSaber/Cube.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour {
+
+    [SerializeField] private CubeSpeed cubeSpeed;
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += Time.deltaTime * transform.forward * cubeSpeed.Speed;
+	}
+}
---
+++ BeatSaber/Assets/BeatSaber/CubeSpeed.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CubeSpeed", menuName = "CubeSpeed")]
+public class CubeSpeed : ScriptableObject
+{
+
+    [SerializeField] private float speed;
+
+    public float Speed
+    {
+        get
+        {
+            return speed;
+        }
+        set
+        {
+            speed = value;
+        }
+    }
+}
